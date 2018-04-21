702fb9f4e4fca372d1f580e49543a784a72a75b4
Ryan Evan Wolf <ryanevwolf@gmail.com>
2018-04-21 20:21:21 +0200

    Added arguments for output directory saving of the extracted images instead of hardcoded

--- bin/unpackBumblebeeData.py
+++ bin/unpackBumblebeeData.py
@@ -7,16 +7,25 @@ import time
 import numpy as np
 ###common ros messages and imports
 import rospy
+import argparse
 
-inputDirectory=sys.argv[1]
+parser =argparse.ArgumentParser()
+parser.add_argument("rawBagFile")
+parser.add_argument("--out",default="/home/ryan/A/")
+args=parser.parse_args()
 
-print("Reading from File =",inputDirectory)
-totalBytes=os.path.getsize(inputDirectory)
+###create directories
+
+if(not os.path.exists(os.path.dirname(args.out))):
+    os.makedirs(os.path.dirname(args.out))
+
+print("Reading from File =",args.rawBagFile)
+totalBytes=os.path.getsize(args.rawBagFile)
 bytesPerImage=2*768*1024
 
 totalImages=int(totalBytes/bytesPerImage)
 remainderBytes=totalBytes%bytesPerImage
-with open(inputDirectory,"rb") as f:
+with open(args.rawBagFile,"rb") as f:
     for imageIndex in range(0,totalImages):
         image=np.zeros((1,bytesPerImage),np.uint8)
         for byteIndex in range(0,bytesPerImage):
@@ -25,7 +34,7 @@ with open(inputDirectory,"rb") as f:
         image=image.reshape(2*768,1024)
         #image=cv2.cvtColor(image,cv2.COLOR_BAYER_BG2RGB)
         imageName="%05d.ppm"%int(imageIndex)
-        name="/home/ryan/DataSets/raw/auto/2/Extracted/"+imageName
+        name=args.out+"/"+imageName
         cv2.imwrite(name,image)
         print(imageIndex,totalImages)
         ##copy an image into a np array
